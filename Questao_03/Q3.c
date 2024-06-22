34a4aaad1c5fae334f4723e2c8c4fd45761ef2a9
Caio Viana <caio.devwork@gmail.com>
2024-06-22 20:54:21 -0300

    main_list foi ordenada, mas a secondary nao esta funcionando

--- Questao_03/Q3.c
+++ Questao_03/Q3.c
@@ -34,9 +34,14 @@ void insert_main_list (Doubly_linked_list* list, double value);
 void read_main_list (Doubly_linked_list* list);
 void read_secondary_list (Singly_linked_list* list);
 
-void merge_sort (Doubly_linked_node* head);
-void partition (Doubly_linked_node* current, Doubly_linked_node* front, Doubly_linked_node* back);
-Doubly_linked_node* merge_lists (Doubly_linked_node* a, Doubly_linked_node* b);
+void merge_sort (Doubly_linked_list* list);
+void merge_sort_main (Doubly_linked_node** head);
+void partition_main (Doubly_linked_node* current, Doubly_linked_node** front, Doubly_linked_node** back);
+Doubly_linked_node* merge_lists_main (Doubly_linked_node* a, Doubly_linked_node* b);
+
+void merge_sort_secondary (Singly_linked_node** head);
+void partition_secondary (Singly_linked_node* current, Singly_linked_node** front, Singly_linked_node** back);
+Singly_linked_node* merge_lists_secondary (Singly_linked_node* a, Singly_linked_node* b);
 
 int main() {
     FILE* file = fopen("L1Q3.in", "r");
@@ -52,7 +57,7 @@ int main() {
         int i = 0;
         while (buffer[i] != 'I') {
             
-            if (buffer[i] >= '0' && buffer[i] <= '9') {
+            if (buffer[i] >= '0' && buffer[i] <= '9' || buffer[i] == '-') {
                 int j = 0;
                 char temp[10];
                 while (buffer[i] != ' ') {
@@ -67,7 +72,7 @@ int main() {
         }
         while (buffer[i] != '\0') {
 
-            if (buffer[i] >= '0' && buffer[i] <= '9') {
+            if (buffer[i] >= '0' && buffer[i] <= '9' || buffer[i] == '-') {
                 int j = 0;
                 char temp[10];
                 while (buffer[i] != ' ' && buffer[i] != '\0') {
@@ -80,7 +85,7 @@ int main() {
             }
             else i++;
         }
-    //merge_sort (main_list->head);
+    merge_sort (main_list);
     read_main_list (main_list);
     free(main_list);
     }
@@ -124,7 +129,6 @@ void create_singly_circular_linked_Node (Singly_linked_list* list, double value)
 
     if (list->head == NULL) {
         list->tail = list->head = new_node;
-        //list->tail = new_node;
         new_node->next = list->head;
     }
     else {
@@ -139,8 +143,15 @@ void insert_main_list (Doubly_linked_list* list, double value) {
     current = list->head;
 
     while (current != NULL) {
-        if (value > current->num && value < current->num + 1) {
+        if (value >= 0) {
+            if (value > current->num && value < current->num + 1) {
             create_singly_circular_linked_Node (current->secondary_list, value);
+            }
+        }
+        else {
+            if (value < current->num && value > current->num - 1) {
+            create_singly_circular_linked_Node (current->secondary_list, value);
+            }
         }
         current = current->next;
     }
@@ -181,62 +192,130 @@ void read_secondary_list (Singly_linked_list* list) {
     free(current);
 }
 
-// mergesort
+// mergesort_GERAL  gcc Q3.c -o Q3.exe
 
-void partition (Doubly_linked_node* current, Doubly_linked_node* front, Doubly_linked_node* back) {
+void merge_sort (Doubly_linked_list* list) {
+
+    Doubly_linked_node* current = (Doubly_linked_node*) malloc(sizeof(Doubly_linked_node));
+    current = list->head;
+
+    /* while(current != NULL) {
+        merge_sort_secondary (&current->secondary_list->head);
+        current = current->next;
+    } */
+    merge_sort_main (&list->head);
+    free(current);
+}
+
+// mergesort_main_list
+
+void merge_sort_main (Doubly_linked_node** head) {
+
+    Doubly_linked_node* current = *head;
+    Doubly_linked_node* a = NULL;
+    Doubly_linked_node* b = NULL;
+
+    if (!current || !current->next) {return;}
+
+    partition_main (current, &a, &b);
+
+    merge_sort_main (&a);
+    merge_sort_main (&b);
+
+    *head = merge_lists_main (a, b);
+}
+
+void partition_main (Doubly_linked_node* current,
+                Doubly_linked_node** front, Doubly_linked_node** back) {
     
     Doubly_linked_node* slow;
     Doubly_linked_node* fast;
 
-    if (current == NULL || current->next == NULL) {
-        front = current;
-        back = NULL;
-    }
-    else {
-        slow = current;
-        fast = current->next;
-        while ( fast != NULL) {
-            fast = fast->next;
-            if (fast != NULL) {
-                slow = slow->next;
-                fast = fast->next;
-            }
+    slow = current;
+    fast = current;
+    while (fast->next && fast->next->next) {
+        if (fast != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        front = current;
-        back= slow->next;
-        slow->next = NULL;
     }
+    *front = current;
+    *back= slow->next;
+    slow->next = NULL;
 }
 
-Doubly_linked_node* merge_lists (Doubly_linked_node* a, Doubly_linked_node* b) {
-    Doubly_linked_node* merged_list = NULL;
+Doubly_linked_node* merge_lists_main (Doubly_linked_node* a, Doubly_linked_node* b) {
+    Doubly_linked_node* result = NULL;
 
     if (a == NULL) {return b;}
     else if (b == NULL) {return a;}
 
     if (a->num <= b->num) {
-        merged_list = a;
-        merged_list->next = merge_lists (a->next, b);
+        result = a;
+        result->next = merge_lists_main (a->next, b);
+        result->next->prev = result;
+        result->prev = NULL;
     }
     else {
-        merged_list = b;
-        merged_list->next = merge_lists (a, b->next);
+        result = b;
+        result->next = merge_lists_main (a, b->next);
+        result->next->prev = result;
+        result->prev = NULL;
     }
-    return merged_list;
+    return (result);
 }
 
-void merge_sort (Doubly_linked_node* head) {
+// mergesort_secondary_list
 
-    Doubly_linked_node* current = head;
-    Doubly_linked_node* a = NULL;
-    Doubly_linked_node* b = NULL;
+void merge_sort_secondary (Singly_linked_node** head) {
+
+    Singly_linked_node* current = *head;
+    Singly_linked_node* a = NULL;
+    Singly_linked_node* b = NULL;
 
     if (current == NULL || current->next == NULL) {return;}
 
-    partition (current, a, b);
+    partition_secondary (current, &a, &b);
 
-    merge_sort (a);
-    merge_sort (b);
+    merge_sort_secondary (&a);
+    merge_sort_secondary (&b);
 
-    head = merge_lists (a, b);
+    *head = merge_lists_secondary (a, b);
+}
+
+void partition_secondary (Singly_linked_node* current,
+                Singly_linked_node** front, Singly_linked_node** back) {
+    
+    Singly_linked_node* slow;
+    Singly_linked_node* fast;
+
+    slow = current;
+    fast = current->next;
+    while ( fast != NULL) {
+        fast = fast->next;
+        if (fast != NULL) {
+            slow = slow->next;
+            fast = fast->next;
+        }
+    }
+    *front = current;
+    *back= slow->next;
+    slow->next = NULL;
+}
+
+Singly_linked_node* merge_lists_secondary (Singly_linked_node* a, Singly_linked_node* b) {
+    Singly_linked_node* result = NULL;
+
+    if (a == NULL) {return b;}
+    else if (b == NULL) {return a;}
+
+    if (a->num <= b->num) {
+        result = a;
+        result->next = merge_lists_secondary (a->next, b);
+    }
+    else {
+        result = b;
+        result->next = merge_lists_secondary (a, b->next);
+    }
+    return (result);
 }
