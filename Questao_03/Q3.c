4eb14f571b710e13cc0ecc2be2b20c98043ca968
Caio Viana <caio.devwork@gmail.com>
2024-07-19 16:39:44 -0300

    Questão 3 modificada e corrigida

--- Questao_03/Q3.c
+++ Questao_03/Q3.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#define TAM 100
+#define TAM_BUFFER 1000
 
 typedef struct _singlylinkednode {
     double num;
@@ -26,58 +28,68 @@ typedef struct _linkedlist {
     Doubly_linked_node* tail;
 } Doubly_linked_list;
 
+typedef struct _Multi {
+    char* stringNum[TAM];
+    int* main_array;
+    double* secondary_array;
+} Multi;
 
 void init_doubly_linked_list (Doubly_linked_list* list);
 void init_singly_linked_list (Singly_linked_list* list);
-void create_doubly_linked_Node (Doubly_linked_list* list, int num);
+void create_doubly_linked_Node (Doubly_linked_list* list, int* array, int size);
 void create_singly_circular_linked_Node (Singly_linked_list* list, double num, char* string);
-void insert_main_list (Doubly_linked_list* list, double value, char* string);
+char*find_string (char**string, double value);
+void insert_main_list (Doubly_linked_list* list, double* array, int size, char**stringNum);
 void write_output (Doubly_linked_list* list);
 void write_second_list (Singly_linked_list* list);
-
-void merge_sort (Doubly_linked_list* list);
-void merge_sort_main (Doubly_linked_node** head);
-void partition_main (Doubly_linked_node* current, Doubly_linked_node** front, Doubly_linked_node** back);
-Doubly_linked_node* merge_lists_main (Doubly_linked_node* a, Doubly_linked_node* b);
-
-void merge_sort_secondary (Singly_linked_node** head);
-void partition_secondary (Singly_linked_node* current, Singly_linked_node** front, Singly_linked_node** back);
-Singly_linked_node* merge_lists_secondary (Singly_linked_node* a, Singly_linked_node* b);
-
+void insertion_sort (int* array_1, int length1, double* array_2, int length2);
+void printArray (int* array_1, int size1, double* array_2, int size2);
+void free_stringNum (char**stringNum, int size);
 void free_memory (Doubly_linked_list* list);
 
 FILE* file;
 FILE* output;
 
 int main() { // L1Q3.in / examples_1.in / examples_2.in
-    file = fopen("examples_2.in", "r");
+    file = fopen("L1Q3.in", "r");
     if (file == NULL) {
         printf("file failed to open!");
         return 1;
     }
-    output = fopen("examples_2.out", "w+");
+    output = fopen("L1Q3.out", "w+");
     if (file == NULL) {
         printf("output file failed to open!");
         return 1;
     }
 
-    char buffer[1000];
+    char buffer[TAM_BUFFER];
     while (fgets(buffer, sizeof(buffer), file) != NULL) {
+
         Doubly_linked_list* main_list = (Doubly_linked_list*) malloc(sizeof(Doubly_linked_list));
         init_doubly_linked_list (main_list);
+
+        char**stringNum = malloc(sizeof(char*) * TAM);
+        int*main_array = (int*)malloc(sizeof(int) * TAM);
+        double*secondary_array = (double*)malloc(sizeof(double) * TAM);
+        int size_stringNum = 0;
+        int size_main = 0;
+        int size_secondary = 0;
+
         int i = 0;
         while (buffer[i] != 'I') {
             
             if (buffer[i] >= '0' && buffer[i] <= '9' || buffer[i] == '-') {
                 int j = 0;
-                char temp[10];
+                char* temp = malloc(sizeof(char) * 12);
                 while (buffer[i] != ' ') {
                     temp[j] = buffer[i];
                     temp[j + 1] = '\0';
                     j++;
                     i++;
                 }
-                create_doubly_linked_Node (main_list, atoi(temp));
+                main_array[size_main] = atoi(temp);
+                size_main++;
+                free(temp);
             }
             else i++;
         }
@@ -85,20 +97,32 @@ int main() { // L1Q3.in / examples_1.in / examples_2.in
 
             if (buffer[i] >= '0' && buffer[i] <= '9' || buffer[i] == '-') {
                 int j = 0;
-                char temp[10];
+                char* temp = malloc(sizeof(char) * 12);
                 while (buffer[i] != ' ' && buffer[i] != '\0' && buffer[i] != '\n') {
                     temp[j] = buffer[i];
                     temp[j + 1] = '\0';
                     j++;
                     i++;
                 }
-                insert_main_list (main_list, atof(temp), temp);
+                stringNum[size_stringNum] = malloc(sizeof(char) * 12);
+                strcpy(stringNum[size_stringNum], temp);
+                size_stringNum++;
+                stringNum[size_stringNum] = NULL;
+                secondary_array[size_secondary] = atof(temp);
+                size_secondary++;
+                free(temp);
             }
             else i++;
         }
-    merge_sort (main_list);
-    write_output (main_list);
-    free_memory (main_list);
+        insertion_sort(main_array, size_main, secondary_array, size_secondary);
+        create_doubly_linked_Node (main_list, main_array, size_main);
+        insert_main_list (main_list, secondary_array, size_secondary, stringNum);
+
+        write_output (main_list);
+        free_memory (main_list);
+        free(secondary_array);
+        free(main_array);
+        free_stringNum(stringNum, size_stringNum);
     }
     fclose(file);
     fclose(output);
@@ -115,23 +139,64 @@ void init_singly_linked_list (Singly_linked_list* list) {
     list->tail = NULL;
 }
 
-void create_doubly_linked_Node (Doubly_linked_list* list, int value) {
-    Doubly_linked_node* new_node = (Doubly_linked_node*) malloc(sizeof(Doubly_linked_node));
-    new_node->secondary_list = (Singly_linked_list*) malloc(sizeof(Singly_linked_list));
-    new_node->num = value;
-    init_singly_linked_list (new_node->secondary_list);
+void insertion_sort (int* array_1, int length1, double* array_2, int length2) {
 
-    if (list->head == NULL) {
-        list->head = new_node;
-        new_node->prev = NULL;
-        list->tail = new_node;
-        new_node->next = NULL;
+    for (int i = 0; i < length1; i++) {
+        int key = array_1[i];
+        int j = i - 1;
+        while (j >= 0 && array_1[j] > key) {
+            array_1[j + 1] = array_1[j];
+            j--;
+        }
+        array_1[j + 1] = key;
     }
-    else {
-        list->tail->next = new_node;
-        new_node->prev = list->tail;
-        new_node->next = NULL;
-        list->tail = new_node;
+
+    for (int i = 0; i < length2; i++) {
+        double key = array_2[i];
+        int j = i - 1;
+        while (j >= 0 && array_2[j] > key) {
+            array_2[j + 1] = array_2[j];
+            j--;
+        }
+        array_2[j + 1] = key;
+    }
+}
+
+void printArray (int* array_1, int size1, double* array_2, int size2) {
+    printf("\n");
+    printf("inteiros ");
+    for (int i = 0; i < size1; i++) {
+        printf("%d ", *(array_1 + i));
+    }
+    printf("\n");
+    printf("floats ");
+    for (int i = 0; i < size2; i++) {
+        printf("%.2lf ", *(array_2 + i));
+    }
+    printf("\n");
+}
+
+void create_doubly_linked_Node (Doubly_linked_list* list, int* array, int size) {
+
+    for (int i = 0; i < size; i++) {
+        
+        Doubly_linked_node* new_node = (Doubly_linked_node*) malloc(sizeof(Doubly_linked_node));
+        new_node->secondary_list = (Singly_linked_list*) malloc(sizeof(Singly_linked_list));
+        new_node->num = array[i];
+        init_singly_linked_list (new_node->secondary_list);
+    
+        if (list->head == NULL) {
+            list->head = new_node;
+            new_node->prev = NULL;
+            list->tail = new_node;
+            new_node->next = NULL;
+        }
+        else {
+            list->tail->next = new_node;
+            new_node->prev = list->tail;
+            new_node->next = NULL;
+            list->tail = new_node;
+        }
     }
 }
 
@@ -151,20 +216,34 @@ void create_singly_circular_linked_Node (Singly_linked_list* list, double value,
     }
 }
 
-void insert_main_list (Doubly_linked_list* list, double value, char* string) {
+char* find_string (char**string, double value) {
+    int i = 0;
+    while (string[i] != NULL) {
+        if (atof(string[i]) == value) {
+            return string[i];
+        }
+        i++;
+    }
+    return NULL;
+}
+
+void insert_main_list (Doubly_linked_list* list, double* array, int size, char**stringNum) {
     Doubly_linked_node* current = (Doubly_linked_node*) malloc(sizeof(Doubly_linked_node));
     current = list->head;
 
+    int i = 0;
     while (current != NULL) {
-        if (value >= 0) {
-            if (value > current->num && value < current->num + 1) {
-            create_singly_circular_linked_Node (current->secondary_list, value, string);
+        for (; i < size; i++) {
+            double value = array[i];
+            char*string;
+            if (find_string(stringNum, value) != NULL) {
+                string = find_string(stringNum, value);
             }
-        }
-        else {
-            if (value < current->num && value > current->num - 1) {
-            create_singly_circular_linked_Node (current->secondary_list, value, string);
+
+           if (value > current->num - 1 && value < current->num + 1) {
+                create_singly_circular_linked_Node (current->secondary_list, value, string);
             }
+            if (value >= current->num + 1) { break; }
         }
         current = current->next;
     }
@@ -198,6 +277,7 @@ void write_second_list (Singly_linked_list* list) {
     bool control = true;
     while (control) {
         fprintf(output, "%s", current->string);
+        //fprintf(output, "%.2lf", current->num);
         if (current != list->tail) {fprintf(output, "->");}
         if (current == list->tail) {control = false;}
         current = current->next;
@@ -205,152 +285,13 @@ void write_second_list (Singly_linked_list* list) {
     free(current);
 }
 
-// mergesort_GERAL  gcc Q3.c -o Q3.exe
-
-void merge_sort (Doubly_linked_list* list) {
-
-    merge_sort_main (&list->head);
-
-    Doubly_linked_node* current = (Doubly_linked_node*) malloc(sizeof(Doubly_linked_node));
-    current = list->head;
-
-    while(current != NULL) {
-        if (current->secondary_list->head != NULL &&
-            current->secondary_list->head != current->secondary_list->tail) {
-            
-            current->secondary_list->tail->next = NULL;
-            merge_sort_secondary (&current->secondary_list->head);
-
-            Singly_linked_node* tail = current->secondary_list->head;
-            while (tail->next != NULL) {
-                tail = tail->next;
-            }
-            current->secondary_list->tail = tail;
-            current->secondary_list->tail->next = current->secondary_list->head;
-            /* if (current->secondary_list->tail->next != NULL) {
-                current->secondary_list->tail = current->secondary_list->tail->next;
-                current->secondary_list->tail->next = current->secondary_list->head;
-            } */
-        }
-        current = current->next;
-    }
-    free(current);
-}
-
-// mergesort_main_list
-
-void merge_sort_main (Doubly_linked_node** head) {
-
-    Doubly_linked_node* current = *head;
-    Doubly_linked_node* a = NULL;
-    Doubly_linked_node* b = NULL;
-
-    if (!current || !current->next) {return;}
-
-    partition_main (current, &a, &b);
-
-    merge_sort_main (&a);
-    merge_sort_main (&b);
-
-    *head = merge_lists_main (a, b);
-}
-
-void partition_main (Doubly_linked_node* current,
-                Doubly_linked_node** front, Doubly_linked_node** back) {
-    
-    Doubly_linked_node* slow;
-    Doubly_linked_node* fast;
-
-    slow = current;
-    fast = current;
-    while (fast->next && fast->next->next) {
-        if (fast != NULL) {
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-    }
-    *front = current;
-    *back= slow->next;
-    slow->next = NULL;
-}
-
-Doubly_linked_node* merge_lists_main (Doubly_linked_node* a, Doubly_linked_node* b) {
-    Doubly_linked_node* result = NULL;
-
-    if (a == NULL) {return b;}
-    else if (b == NULL) {return a;}
-
-    if (a->num <= b->num) {
-        result = a;
-        result->next = merge_lists_main (a->next, b);
-        result->next->prev = result;
-        result->prev = NULL;
-    }
-    else {
-        result = b;
-        result->next = merge_lists_main (a, b->next);
-        result->next->prev = result;
-        result->prev = NULL;
-    }
-    return (result);
-}
-
-// mergesort_secondary_list
-
-void merge_sort_secondary (Singly_linked_node** head) {
-
-    Singly_linked_node* current = *head;
-    Singly_linked_node* a = NULL;
-    Singly_linked_node* b = NULL;
-
-    if (current == NULL || current->next == NULL) {return;}
-
-    partition_secondary (current, &a, &b);
-
-    merge_sort_secondary (&a);
-    merge_sort_secondary (&b);
-
-    *head = merge_lists_secondary (a, b);
-}
-
-void partition_secondary (Singly_linked_node* current,
-                          Singly_linked_node** front, Singly_linked_node** back) {
-    
-    Singly_linked_node* slow;
-    Singly_linked_node* fast;
-
-    slow = current;
-    fast = current->next;
-    while (fast != NULL) {
-        fast = fast->next;
-        if (fast != NULL) {
-            slow = slow->next;
-            fast = fast->next;
-        }
-    }
-    *front = current;
-    *back= slow->next;
-    slow->next = NULL;
-}
-
-Singly_linked_node* merge_lists_secondary (Singly_linked_node* a, Singly_linked_node* b) {
-    Singly_linked_node* result = NULL;
-
-    if (a == NULL) {return b;}
-    else if (b == NULL) {return a;}
-
-    if (a->num <= b->num) {
-        result = a;
-        result->next = merge_lists_secondary (a->next, b);
-    }
-    else {
-        result = b;
-        result->next = merge_lists_secondary (a, b->next);
-    }
-    return (result);
-}
-
 // libera a memória alocada
+
+void free_stringNum (char**stringNum, int size) {
+    for (int i = 0; i < size; i++) {
+        free(stringNum[i]);
+    }
+}
 
 void free_memory (Doubly_linked_list* list) {
 
